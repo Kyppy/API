102cd01691269706cf5c37a65225dc800ce84963
Kyppy Simani <simanikyppy@gmail.com>
2018-11-28 22:19:14 +0300

    [finishes #162278744] Add POST resource

--- app/api/v1/redflagviews.py
+++ app/api/v1/redflagviews.py
@@ -11,6 +11,13 @@ class RedFlag(Resource, RedFlagModel):
     def get(self, red_flag_id):
         incident = next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None)
         return incident, 200 if incident else 404
+	
+	def post(self, red_flag_id):
+        if next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None):
+            return {"message":"A red-flag with id '{}' already exists.".format(red_flag_id)}, 400
+        data = request.get_json(silent=True)
+        self.incidents.store(data)
+        return self.incidents.db
     
 class Red_flags(Resource):
 
