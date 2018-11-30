43ac63b41f865e44e8e6a5d5aa2b260a4cc7054f
Kyppy Simani <simanikyppy@gmail.com>
2018-11-30 15:19:21 +0300

    Fixed delete resource error.Updated travis CI test path

--- app/api/v1/redflagviews.py
+++ app/api/v1/redflagviews.py
@@ -3,52 +3,44 @@ from flask_restful import Resource, Api
 
 from .redflagmodels import RedFlagModel
 
+incidents = RedFlagModel()
 class RedFlag(Resource, RedFlagModel):
-
-    def __init__(self):
-        self.incidents = RedFlagModel()
-    
+   
     def get(self, red_flag_id):
-        incident = next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None)
+        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
         return {"status":200,"data":incident} if incident else {"status":404,"message":"An incident with id '{}' does not exist.".format(red_flag_id)}
     
     def post(self, red_flag_id):
         data = request.get_json(silent=True)
         if data['id'] != red_flag_id:
             return {"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
-        if next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None):
+        if next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None):
             return {"message":"A red-flag with id '{}' already exists.".format(red_flag_id)}, 400
 
-        self.incidents.store(data)
+        incidents.store(data)
         return {"status":201,"data":{"id":red_flag_id,"message":"created red-flag record"}}
     
     def delete(self, red_flag_id):
-        incident = next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None)
+        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
         if incident == None:
             return {"status":404,"message":"An incident with id '{}' does not exist.".format(red_flag_id)}
         else:
-            self.incidents.db = list(filter(lambda x: x['id'] != red_flag_id, self.incidents.db))
+            incidents.db = list(filter(lambda x: x['id'] != red_flag_id, incidents.db))
             return {"status":200,"data":{"id":red_flag_id,"message":"red-flag record has been deleted"}}
     
 class RedFlags(Resource):
-
-    def __init__(self):
-        self.incidents = RedFlagModel()
-
+   
     def get(self):
-        return{"status":200,"data":self.incidents.db} 
+        return{"status":200,"data":incidents.db} 
 
 class PatchLocation(Resource):
-
-    def __init__(self):
-        self.incidents = RedFlagModel()
 
     def patch(self, red_flag_id):
         data = request.get_json(silent=True)
         if data['id'] != red_flag_id:
             return{"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
 
-        incident = next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None)
+        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
         if incident is None:
             return{"message":"A red-flag incident with id '{}' does not exist.".format(red_flag_id)}, 404
 
@@ -61,14 +53,11 @@ class PatchLocation(Resource):
 
 class PatchComment(Resource):
 
-    def __init__(self):
-        self.incidents = RedFlagModel()
-
     def patch(self, red_flag_id):
         data = request.get_json(silent=True)
         if data['id'] != red_flag_id:
             return{"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
-        incident = next(filter(lambda x: x['id'] == red_flag_id, self.incidents.db), None)
+        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
         if incident is None:
             return{"message":"A red-flag incident with id '{}' does not exist.".format(red_flag_id)}, 404
 
