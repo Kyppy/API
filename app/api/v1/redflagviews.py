6573ecdf0bee43ca26e1cc9c77cba5ca4328a82a
Kyppy Simani <simanikyppy@gmail.com>
2018-12-04 00:30:54 +0300

    cleared reflagviews to pinpoint bug

--- app/api/v1/redflagviews.py
+++ app/api/v1/redflagviews.py
@@ -5,66 +5,8 @@ from .redflagmodels import RedFlagModel
 
 incidents = RedFlagModel()
 class RedFlag(Resource, RedFlagModel):
-   
-    def get(self, red_flag_id):
-        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
-        return {"status":200,"data":incident} if incident else {"status":404,"message":"An incident with id '{}' does not exist.".format(red_flag_id)}
-    
-    def post(self, red_flag_id):
-        data = request.get_json(silent=True)
-        if data['id'] != red_flag_id:
-            return {"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
-        if next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None):
-            return {"message":"A red-flag with id '{}' already exists.".format(red_flag_id)}, 400
+    pass
 
-        incidents.store(data)
-        return {"status":201,"data":{"id":red_flag_id,"message":"created red-flag record"}}
-    
-    def delete(self, red_flag_id):
-        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
-        if incident == None:
-            return {"status":404,"message":"An incident with id '{}' does not exist.".format(red_flag_id)}
-     
-        incidents.db = list(filter(lambda x: x['id'] != red_flag_id, incidents.db))
-        return {"status":200,"data":{"id":red_flag_id,"message":"red-flag record has been deleted"}}
-    
-class RedFlags(Resource):
-   
-    def get(self):
-        return{"status":200,"data":incidents.db} 
-
-class PatchLocation(Resource):
-
-    def patch(self, red_flag_id):
-        data = request.get_json(silent=True)
-        if data['id'] != red_flag_id:
-            return{"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
-
-        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
-        if incident is None:
-            return{"message":"A red-flag incident with id '{}' does not exist.".format(red_flag_id)}, 404
-
-        if data['location'] == None:
-            return{"message":"No location patch data was sent."}, 404
-
-        incident['location']= data['location']
-        return {"status":200,"data":{"id":red_flag_id,"message":"Updated red-flag record's location"}}
-
-class PatchComment(Resource):
-
-    def patch(self, red_flag_id):
-        data = request.get_json(silent=True)
-        if data['id'] != red_flag_id:
-            return{"message":"'id'{} provided by request body and 'id' {} provided by url do not match".format(data['id'],red_flag_id)}, 400
-        incident = next(filter(lambda x: x['id'] == red_flag_id, incidents.db), None)
-        if incident is None:
-            return{"message":"A red-flag incident with id '{}' does not exist.".format(red_flag_id)}, 404
-
-        if data['comment'] == None:
-            return{"message":"No comment patch data was sent."}, 400
-        
-        incident['comment']= data['comment']
-        return {"status":200,"data":{"id":red_flag_id,"message":"Updated red-flag record's comment"}}
 
 
 
