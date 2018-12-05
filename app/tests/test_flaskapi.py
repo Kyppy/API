516b66e5cff2af265b3fcaaaa4cd705b08a8dc8c
Kyppy Simani <simanikyppy@gmail.com>
2018-12-05 11:36:27 +0300

    Updated test file

--- app/tests/test_flaskapi.py
+++ app/tests/test_flaskapi.py
@@ -46,44 +46,29 @@ class TestUsers(unittest.TestCase):
         response = self.app.patch('/api/v1/red_flag/800/comment', data=json.dumps(self.data), content_type='application/json')
         self.assertEqual(response.status_code,404)
     
-    def test_comment_patch_id_mismatch(self):
-        response = self.app.patch('/api/v1/red_flag/700/comment', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(response.status_code,400)
-    
     def test_empty_location_patch(self):
         response = self.app.patch('/api/v1/red_flag/800/location', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.status_code, 404)
     
     def test_delete_empty_record(self):
-        response = self.app.delete('/api/v1/red_flag/800')
+        response = self.app.delete('/api/v1/red_flag/100')
         result = json.loads(response.data)
-        self.assertEqual(result['status'],404)
+        self.assertEqual(result['status'], 404)
    
     def test_post_record_and_get_record(self):
-        response = self.app.post('/api/v1/red_flag/800', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(response.status_code,200)
+        response = self.app.post('/api/v1/red_flags', data=json.dumps(self.data), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
         response = self.app.get('/api/v1/red_flag/800')
         result = json.loads(response.data)
-        self.assertEqual(result['status'],200)
-    
-    def test_post_record_twice(self):
-        self.app.post('/api/v1/red_flag/800', data=json.dumps(self.data), content_type='application/json')
-        response = self.app.post('/api/v1/red_flag/800', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(response.status_code,400)
-    
+        self.assertEqual(result['status'], 200)
+     
     def test_delete_record(self):
-        self.app.post('/api/v1/red_flag/800', data=json.dumps(self.data), content_type='application/json')
+        self.app.post('/api/v1/red_flags', data=json.dumps(self.data), content_type='application/json')
         response = self.app.delete('/api/v1/red_flag/800')
         result = json.loads(response.data)
-        self.assertEqual(result['status'],200)
+        self.assertEqual(result['status'], 200)
     
-    def test_post_record_id_mismatch(self):
-        response = self.app.post('/api/v1/red_flag/700', data=json.dumps(self.data), content_type='application/json')
-        self.assertEqual(response.status_code,400)
     
-"""
-Arbitrary change to test Coveralls.Change
-"""
     
       
 
